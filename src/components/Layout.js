089d05eaa2102d8fe408c5627f10f501f1cd561d
SIMOHANNANI <mohamed.hanani2019@gmail.com>
2020-10-16 19:33:50 +0100

    First release

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Layout extends React.Component{
+    render(){
+        return(
+            <div className="jumbotron d-flex align-items-center min-vh-100">
+                <div className={'container text-center'}>
+                    <h2>PLEASE SPECIFY THE EXACT LAYOUT FOR THE WEBSITE</h2>
+                </div>
+            </div>
+        )
+    }
+}
+export default Layout

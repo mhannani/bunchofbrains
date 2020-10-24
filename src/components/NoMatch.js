5e7b951484a9532cbe55a8148caa77c712ff4033
SIMOHANNANI <mohamed.hanani2019@gmail.com>
2020-10-24 19:55:37 +0100

    setting up 404 page

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Footer from '../components/Footer/Footer'
-import Page_404 from "./chunks/Page_404";
+import NotFound from "./chunks/NotFound";
 const NoMatch = () =>{
         return(
             <div>
-                <Page_404 className={'_404-item-message text-center'}>
+                <NotFound className={'_404-item-message text-center'}>
                     <h1>Oops!</h1>
                     <h4 className={'mx-2'}>404 - PAGE NOT FOUND</h4>
                     <h6 className={'mx-5'}>The page you are looking for might have been removed had its name changed or is temporarily unavailable</h6>
-                </Page_404>
+                </NotFound>
                 <Footer/>
             </div>
         )
--- src/components/chunks/NotFound.js
+++ src/components/chunks/NotFound.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
-const Page_404 = styled.div`
+const NotFound = styled.div`
   color: ${({ theme }) => theme.text}
 `;
 
-export default Page_404;
+export default NotFound;

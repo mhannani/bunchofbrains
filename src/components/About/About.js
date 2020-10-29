970db610da4af9e8674f34b74b8b535fafb1ef29
SIMOHANNANI <mohamed.hanani2019@gmail.com>
2020-10-29 19:02:41 +0100

    about page .

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,48 @@
-import React from 'react'
-
-class About extends React.Component{
-    render(){
-        return(
-            <div className="container-fluid">
-                <p className="text-center">
-                    About depression website ...
-                </p>
-            </div>
+import React from 'react';
+import Div from '../chunks/Div';
+class About extends React.Component {
+    render() {
+        return (
+            <Div className="container-fluid about">
+                <h2 className={'text-center'}>About</h2>
+                <div className={'text-center mt-5'}>
+                    <p>Bunchofbrains.com is web app chat service that links people who
+                        have triumphed over mental illnesses such as depression,
+                        anxiety or PTSD. Or found better ways to cope with it.
+                    </p>
+                    <p>
+                        So anyone who struggles with life’s challenges can get tips
+                        and advices, anytime, anywhere from people who had experienced
+                        the same challenges and overcome it.
+                    </p>
+                    <p>
+                        Our web app namelessly and safely associates genuine individuals
+                        with a successful experience to audience members in one-on-one talk.
+                    </p>
+                    <p>
+                        Any individual who needs to pick a mind about his mental illness
+                        successful experience can rapidly contact a specific audience
+                        through our organization. We have several audience members who
+                        originate from varying backgrounds and have differing encounters.
+                    </p></div>
+                <h2 className={'text-center mt-5'}>How does it work?</h2>
+                <div className={'text-center mt-5'}>
+                    <p>
+                        The process is straight forward, you choose a user to have a conversation
+                        with over audio or video in complete anonymity.
+                    </p>
+                    <p>
+                        Our users are people who has defeated their mental illnesses and are
+                        willing to share their experience.
+                    </p>
+                    <p>
+                        Join our community today! You will be amazed that you’re not alone
+                        and there is better ways to live life to the fullest.
+                    </p>
+                </div>
+            </Div>
         )
     }
 }
+
 export default About

aab524156c6ab64e3b422c1949d142696470cd7f
SIMOHANNANI <mohamed.hanani2019@gmail.com>
2020-10-30 16:16:09 +0100

    faq page

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,13 +1,39 @@
 import React from 'react'
-
+import Button from "../chunks/Button";
+import Div from "../chunks/Div";
 class Contact extends React.Component{
     render(){
         return(
-            <div className="container-fluid">
-                <p className="text-center">
-                    Contact us by giving us a shot ...
-                </p>
-            </div>
+            <Div className="container contact d-flex justify-content-center">
+
+                <form className={'my-5'}>
+                    <div className={'row d-flex justify-content-center'}>
+                        <div className="form-group col-xl-7 col-lg-8">
+                            <h1>Get in touch</h1>
+                                please fill out the quick form and we will be in touch with lightening speed.
+                        </div>
+
+                        <div className="form-group col-xl-7 col-lg-8">
+                            <input type="text" className="form-control" placeholder="Name"/>
+                        </div>
+
+                        <div className="form-group col-xl-7 col-lg-8">
+                            <input type="email" className="form-control" id="inputEmail4" placeholder="Email"/>
+                        </div>
+
+                        <div className="form-group col-xl-7 col-lg-8">
+                            <textarea className="h-auto form-control rounded-0" id="exampleFormControlTextarea1"
+                                  rows="7"/>
+                        </div>
+
+                        <div className="form-group col-xl-7 col-lg-8">
+                            <Button type="submit" className="resetButton col-xl-12 btn font-weight-bold">Sign in</Button>
+                        </div>
+                    </div>
+
+
+                </form>
+            </Div>
         )
     }
 }
--- src/components/FAQ/Faq.js
+++ src/components/FAQ/Faq.js
@@ -3,10 +3,124 @@ import React from 'react'
 class Faq extends React.Component{
     render(){
         return(
-            <div className="container">
-                <p className="text-center">
-                    Some frequently asked question ...
-                </p>
+            <div className="container faq">
+                <div id="accordion" className={'mb-5'}>
+                    <div className="card mb-2">
+                        <div className="card-header" id="headingOne">
+                            <h5 className="mb-0">
+                                <button className="btn col-12 float-left" data-toggle="collapse" data-target="#collapseOne"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                    Q1- bla bla bla bla bla bla bla bla bla bla ?
+                                </button>
+                            </h5>
+                        </div>
+
+                        <div id="collapseOne" className="collapse" aria-labelledby="headingOne"
+                             data-parent="#accordion">
+                            <div className="card-body">
+                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
+                                squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck
+                                quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it
+                                squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica,
+                                craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
+                                butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth
+                                nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="card mb-2">
+                        <div className="card-header" id="headingOne">
+                            <h5 className="mb-0">
+                                <button className="btn col-12 float-left" data-toggle="collapse" data-target="#collapseTwo"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                    Q2- bla bla bla bla bla bla bla bla bla bla ?
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo"
+                             data-parent="#accordion">
+                            <div className="card-body">
+                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
+                                squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck
+                                quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it
+                                squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica,
+                                craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
+                                butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth
+                                nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card mb-2">
+                        <div className="card-header" id="headingOne">
+                            <h5 className="mb-0">
+                                <button className="btn col-12 float-left" data-toggle="collapse" data-target="#collapseThree"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                    Q3- bla bla bla bla bla bla bla bla bla bla ?
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="collapseThree" className="collapse" aria-labelledby="headingThree"
+                             data-parent="#accordion">
+                            <div className="card-body">
+                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
+                                squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck
+                                quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it
+                                squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica,
+                                craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
+                                butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth
+                                nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card mb-2">
+                        <div className="card-header" id="headingFour">
+                            <h5 className="mb-0">
+                                <button className="btn col-12 float-left" data-toggle="collapse" data-target="#collapseFour"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                    Q3- bla bla bla bla bla bla bla bla bla bla ?
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="collapseFour" className="collapse" aria-labelledby="headingFour"
+                             data-parent="#accordion">
+                            <div className="card-body">
+                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
+                                squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck
+                                quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it
+                                squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica,
+                                craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
+                                butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth
+                                nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card mb-2">
+                        <div className="card-header" id="headingFive">
+                            <h5 className="mb-0">
+                                <button className="btn col-12 float-left" data-toggle="collapse" data-target="#collapseFive"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                    Q3- bla bla bla bla bla bla bla bla bla bla ?
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="collapseFive" className="collapse" aria-labelledby="headingFive"
+                             data-parent="#accordion">
+                            <div className="card-body">
+                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
+                                squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck
+                                quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it
+                                squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica,
+                                craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
+                                butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth
+                                nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }

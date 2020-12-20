b0268c41e66f69b686b4fd7741b3aebd706f1339
SIMOHANNANI <mohamed.hanani2019@gmail.com>
2020-12-20 01:03:42 +0100

    wiring up Addiction page.

--- src/components/All_troubles/troubles_component/Addiction.js
+++ src/components/All_troubles/troubles_component/Addiction.js
@@ -5,11 +5,54 @@ const Addiction = (props)=>{
         document.title = props.title || "";
     })
     return(
-        <div className={'mx-4 addiction'}>
+        <div className={'mx-md-4 article_container'}>
             <GoBackButton/>
-            <div className={'container'}>
-                addiction
-            </div>
+            <article className={'container article'}>
+                <div className={'container'}>
+                    <header>
+                        <h1>An Overview of Addiction</h1>
+                        <h2>What do Addiction do?</h2>
+                    </header>
+                    <div className={'container mx-md-4 img-placeholder'}/>
+                    <div className={'container row'}>
+                        <div className={'mt-3 col-12'}>
+                            <section>
+                                <p>
+                                    From a scientific and medical point of view, addictions are brain pathologies
+                                    defined by an addiction to a substance or an activity,
+                                    with deleterious consequences.
+                                </p>
+
+                                <p>
+                                    Addictions concern tobacco (nicotine), alcohol, cannabis,
+                                    opiates (heroin, morphine), cocaine, amphetamines and synthetic derivatives.
+                                    Among non-substance addictions, only pathological gambling (gambling and gambling)
+                                    is clinically recognized as a behavioral addiction in international
+                                    diagnostic classifications (DSM 5).
+                                </p>
+                                <p>
+                                    The North American Institute of Drugs, the National Institute
+                                    of Drug Abuse (NIDA) defines it as follows:
+                                </p>
+                                <p>
+                                    Addiction is a chronic, relapsing brain condition characterized by compulsive
+                                    drug seeking and use, despite knowledge of its harmful consequences.
+                                </p>
+                                <p>
+                                    Start talking to our volunteers who are willing to share their
+                                    stories about addiction
+
+                                </p>
+                                <p>
+                                    <u>Start here</u>
+                                </p>
+                            </section>
+
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <div className="mx-md-auto"/>
         </div>
     )
 }

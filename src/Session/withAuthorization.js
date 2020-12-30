d37bec57e2348bdf20c9a60885e55bc1bef74254
SIMOHANNANI <mohamed.hanani2019@gmail.com>
2020-12-30 18:24:00 +0100

    Setting up Redux store to manage the current authenticated user.

--- src/Session/withAuthorization.js
+++ src/Session/withAuthorization.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
+import { connect } from 'react-redux';
 import { withFirebase } from '../Firebase';
-import AuthUserContext from "../Session/context"
 
-const withAuthorization = condition => Component => {
+const withAuthorization = (condition) => Component => {
     class WithAuthorization extends React.Component {
         componentDidMount() {
             this.listener = this.props.firebase.auth.onAuthStateChanged(
@@ -24,19 +24,19 @@ const withAuthorization = condition => Component => {
         }
 
         render() {
-            return (
-                <AuthUserContext.Consumer>
-                    {authUser =>
-                        condition(authUser) ? <Component {...this.props} /> : null
-                    }
-                </AuthUserContext.Consumer>
-            );
+            return condition(this.props.authUser) ? (
+                <Component {...this.props} />
+            ) : null;
         }
     }
+    const mapStateToProps = state => ({
+        authUser: state.sessionState.authUser,
+    });
 
     return compose(
         withRouter,
         withFirebase,
+        connect(mapStateToProps),
     )(WithAuthorization);
 };
 
--- src/components/Header/UserActions.js
+++ src/components/Header/UserActions.js
@@ -1,74 +1,59 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {useSelector} from 'react-redux';
 import 'antd/dist/antd.css';
-import { Menu, Dropdown } from 'antd';
-import { withRouter } from "react-router";
-import {UserOutlined, DashboardOutlined, ProfileOutlined, MessageOutlined } from '@ant-design/icons';
+import {Menu, Dropdown} from 'antd';
+import {withRouter} from "react-router";
+import {UserOutlined, DashboardOutlined, ProfileOutlined, MessageOutlined} from '@ant-design/icons';
 import withAuthorization from "../../Session/withAuthorization";
-import AuthUserContext from "../../Session/context";
 import {Link} from "react-router-dom";
-const UserActions = ({ firebase }) => {
 
-
+const UserActions = ({firebase}) => {
+    const authUser = useSelector(state => state.sessionState.authUser)
     const menu = (
         <Menu>
-            <Menu.Item key="1"  icon={<DashboardOutlined />}>
+            <Menu.Item key="1" icon={<DashboardOutlined/>}>
                 <Link to={'/dashboard'}>
                     Dashboard
                 </Link>
             </Menu.Item>
-            <Menu.Item key="2" icon={<ProfileOutlined />}>
+            <Menu.Item key="2" icon={<ProfileOutlined/>}>
                 <Link to={'/profile'}>
                     Profile
                 </Link>
             </Menu.Item>
-            <Menu.Item  key="3" icon={<MessageOutlined />}>
+            <Menu.Item key="3" icon={<MessageOutlined/>}>
                 <Link to={'/messages'}>
-                Messages
+                    Messages
                 </Link>
             </Menu.Item>
-            <Menu.Item onClick={firebase.doSignOut} className={'text-danger'} key="4" icon={<UserOutlined />}>
+            <Menu.Item onClick={firebase.doSignOut} className={'text-danger'} key="4" icon={<UserOutlined/>}>
                 Log out
             </Menu.Item>
         </Menu>
     );
-    return(
-        <AuthUserContext.Consumer>
-        <div>
-            {authUser => (
-                <div className={'container'}>
-                    <Dropdown.Button overlay={menu} placement="bottomRight"
-                                    style={{
-                                        float: 'left',
-                                    }}
-                                    className="dropdown-btn mt-1 my-auto"
-                                    icon={
-                                        <UserOutlined
-                                            className={'bg-red'}
-                                            style={{
-                                                fontSize: '25px',
-                                                // backgroundColor: '#f0f0f0',
-                                                borderRadius: '50%',
-                                            }}
-                                        />
-                                    }
-                >
-                        {authUser.username.toUpperCase()}
-                    </Dropdown.Button>
-                </div>
-            )
-            }
-        </div>
-        </AuthUserContext.Consumer>
 
+    return (
+        <div className={'container'}>
+            <Dropdown.Button overlay={menu} placement="bottomRight"
+                             style={{
+                                 float: 'left',
+                             }}
+                             className="dropdown-btn mt-1 my-auto"
+                             icon={
+                                 <UserOutlined
+                                     className={'bg-red'}
+                                     style={{
+                                         fontSize: '25px',
+                                         // backgroundColor: '#f0f0f0',
+                                         borderRadius: '50%',
+                                     }}
+                                 />
+                             }
+            >
+                {authUser.username.toUpperCase()}
+            </Dropdown.Button>
+        </div>
     )
 }
 const condition = authUser => !!authUser;
-// export default withAuthorization(condition)(withRouter(UserActions));
-
-
-const mapStateToProps = state => ({
-    authUser: state.sessionState.authUser,
-});
-
-export default connect(mapStateToProps)(withAuthorization(condition)(withRouter(UserActions)));
+export default withAuthorization(condition)(withRouter(UserActions));

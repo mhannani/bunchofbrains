bf05e914dca5f257696a87841f5bb8d7af5fbbf1
SIMOHANNANI <mohamed.hanani2019@gmail.com>
2020-12-31 23:01:32 +0100

    Adding user avatar of new users and its operations.

--- src/components/Header/UserActions.js
+++ src/components/Header/UserActions.js
@@ -1,59 +1,135 @@
+// in src/MyUserMenu.js
 import React from 'react';
-import {useSelector} from 'react-redux';
-import 'antd/dist/antd.css';
-import {Menu, Dropdown} from 'antd';
-import {withRouter} from "react-router";
-import {UserOutlined, DashboardOutlined, ProfileOutlined, MessageOutlined} from '@ant-design/icons';
-import withAuthorization from "../../Session/withAuthorization";
+import { withStyles } from '@material-ui/core/styles';
+import Menu from '@material-ui/core/Menu';
 import {Link} from "react-router-dom";
+import Avatar from '@material-ui/core/Avatar';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import withAuthorization from "../../Session/withAuthorization";
+import {withRouter} from "react-router";
+import {useSelector} from 'react-redux';
+import { green } from "@material-ui/core/colors";
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import PersonIcon from '@material-ui/icons/Person';
+import EmailIcon from '@material-ui/icons/Email';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import { useHistory } from 'react-router-dom';
 
-const UserActions = ({firebase}) => {
-    const authUser = useSelector(state => state.sessionState.authUser)
-    const menu = (
-        <Menu>
-            <Menu.Item key="1" icon={<DashboardOutlined/>}>
-                <Link to={'/dashboard'}>
-                    Dashboard
-                </Link>
-            </Menu.Item>
-            <Menu.Item key="2" icon={<ProfileOutlined/>}>
-                <Link to={'/profile'}>
-                    Profile
-                </Link>
-            </Menu.Item>
-            <Menu.Item key="3" icon={<MessageOutlined/>}>
-                <Link to={'/messages'}>
-                    Messages
-                </Link>
-            </Menu.Item>
-            <Menu.Item onClick={firebase.doSignOut} className={'text-danger'} key="4" icon={<UserOutlined/>}>
-                Log out
-            </Menu.Item>
-        </Menu>
-    );
+const StyledMenu = withStyles({
+    paper: {
+        border: '0',
+    },
+})((props) => (
+    <Menu
+        elevation={10}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+        }}
+        transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+        }}
+        {...props}
+    />
+));
 
-    return (
-        <div className={'container'}>
-            <Dropdown.Button overlay={menu} placement="bottomRight"
-                             style={{
-                                 float: 'left',
-                             }}
-                             className="dropdown-btn mt-1 my-auto"
-                             icon={
-                                 <UserOutlined
-                                     className={'bg-red'}
-                                     style={{
-                                         fontSize: '25px',
-                                         // backgroundColor: '#f0f0f0',
-                                         borderRadius: '50%',
-                                     }}
-                                 />
-                             }
-            >
-                {authUser.username.toUpperCase()}
-            </Dropdown.Button>
-        </div>
-    );
+const StyledMenuItem = withStyles((theme) => ({
+    root: {
+        '&:focus': {
+            backgroundColor: green[500],
+            '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+                color: theme.palette.common.white,
+            },
+        },
+    },
+}))(MenuItem);
+
+const UserActions = ({firebase})=> {
+        const authUser = useSelector(state => state.sessionState.authUser);
+        const photoUrl = useSelector(state => state.sessionState.photoURL);
+        const [anchorEl, setAnchorEl] = React.useState(null);
+        const history = useHistory();
+        const handleClick = (event) => {
+            setAnchorEl(event.currentTarget);
+        };
+
+        const handleClose = () => {
+            setAnchorEl(null);
+        };
+        const goToDashboard = () => {
+            history.push("/dashboard");
+            setAnchorEl(null)
+        }
+        const goToProfile = () => {
+            history.push("/profile");
+            setAnchorEl(null)
+        }
+        const goToMessages = () => {
+            history.push("/messages");
+            setAnchorEl(null)
+        }
+
+        return (
+            <div>
+                <div>
+                    <div className={'d-flex align-items-center row mui-fixed'}>
+                        <div>
+                            {authUser.username}
+                        </div>
+                        <div className={'mx-3 gradient-avatar'}>
+                            <Avatar aria-controls="customized-menu"
+                                    aria-haspopup="true"
+                                    variant="contained"
+                                    color="transparent"
+                                    onClick={handleClick}
+                                    src={photoUrl}/>
+                        </div>
+                    </div>
+
+                    <StyledMenu
+                        arrow
+                        id="customized-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                        disableScrollLock={true}
+                    >
+                        <StyledMenuItem  onClick={goToDashboard}>
+                            <Link to={'/dashboard'}>
+                                <ListItemIcon>
+                                    <DashboardIcon fontSize="small" />
+                                </ListItemIcon>
+                            </Link>
+                            <ListItemText primary="Dashboard" />
+                        </StyledMenuItem>
+                        <StyledMenuItem onClick={goToProfile}>
+                            <ListItemIcon>
+                                <PersonIcon fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText primary="Profile" />
+                        </StyledMenuItem>
+                        <StyledMenuItem  onClick={goToMessages}>
+                            <ListItemIcon>
+                                <EmailIcon fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText primary="Messages" />
+                        </StyledMenuItem>
+                        <StyledMenuItem onClick={firebase.doSignOut}>
+                            <ListItemIcon>
+                                <ExitToAppIcon fontSize="very-small" />
+                            </ListItemIcon>
+                            <ListItemText primary="Log out" />
+                        </StyledMenuItem>
+                    </StyledMenu>
+                </div>
+            </div>
+        );
 }
+
 const condition = authUser => !!authUser;
 export default withAuthorization(condition)(withRouter(UserActions));
